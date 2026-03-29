48f1fbd375f3e9ff1bb9b24368f9c2b658448a7b
Rohit Kathiravan <rohitkathir@Rohits-MacBook-Air.local>
2026-03-30 00:58:32 +0530

    fix port for render

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 
 // Middleware
 const cors = require("cors")
